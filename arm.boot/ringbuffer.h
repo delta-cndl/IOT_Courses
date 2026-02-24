e06d27a9f67c807a8da3a995eb1ccf13604664f0
Cheikh Gaye Coundoul <cheikhcndl55@gmail.com>
2026-02-24 23:25:06 +0100

    timer

---
+++ arm.boot/ringbuffer.h
@@ -0,0 +1,20 @@
+#ifndef RINGBUFFER_H
+#define RINGBUFFER_H
+
+#include <stdint.h>
+
+#define RB_SIZE 128
+
+typedef struct {
+    uint8_t data[RB_SIZE];
+    uint32_t head;
+    uint32_t tail;
+} ringbuffer_t;
+
+void rb_init(ringbuffer_t* rb);
+int rb_empty(ringbuffer_t* rb);
+int rb_full(ringbuffer_t* rb);
+void rb_put(ringbuffer_t* rb, uint8_t c);
+uint8_t rb_get(ringbuffer_t* rb);
+
+#endif
